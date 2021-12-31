02b199a4709f7988f3a4cef7a268a6a533b42b83
Harnoor Kalra <harnoor29@yahoo.com>
2021-12-31 16:39:27 -0700

    First build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # budget-notifications
-This code takes the salary input and allocates a portion of your salary towards your bills, food, misc items and sends you a desktop notification, also reminds you to pay your bills. (Compatible only with MacOS - M1 included)
+
+Hi everyone, this code basically takes the salary input and allocates a portion of your salary towards your bills, food, misc items and sends you a desktop notification, also reminds you to pay your bills. (Compatible only with MacOS - M1 included)
+I have also provided my template of the budgeting sheet from where my code pulls the data. You can use that or make a new one of your own. Don't forget to change the path of the directory in which your budgeting sheet is present!
---
+++ notificationsApp.py
@@ -0,0 +1,56 @@
+import time
+import pync
+import pandas as pd
+from datetime import date
+
+salary = float(input("Enter this month's salary: ",))
+file = 'budgeting.xlsx'
+df = pd.read_excel('/Users/noorkalra/Desktop/notifications/budgeting.xlsx')
+df.drop(labels = ['Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7'], axis = 1, inplace= True)
+df.drop(labels = [17], axis = 0, inplace= True)
+today = date.today()
+
+def Food():
+    if df.at[16,'Food'] >= 0.3*salary and df.at[16,'Food'] <= 0.35*salary:
+        title = "Too much spent on Food"
+        message = "Please stop spending, you're approaching the food limit for this month which is {}".format(round(salary*0.4),1)
+        pync.notify(message)
+    if df.at[16,'Food'] == 0.4*salary:
+        title = "You have reached your limit for food."
+        message = "Please stop now, you have reached your food limit for this month. You'll be broke."
+        pync.notify(message)
+    else:
+        title = "You have exceeded the food limit"
+        message = "You have spent {} on food this month. You have exceeded limit for the month, STOP SPENDING!!".format(round(df.at[16, 'Food']),1)
+        pync.notify(message)
+
+def Bills():
+    d = today.day
+    if d > 20:
+        title = "Bills overdue!"
+        message = "Bills were due on the 20th!! Pay them off if you haven't already!!!"
+        pync.notify(message)
+    if d == 19:
+        title = "Bills due tomorrow!"
+        message = "Get that money out. Bills are due tomorrow!"
+        pync.notify(message)
+
+
+def misc():
+    if df.at[16, 'Misc'] >= salary*0.2 and df.at[16, 'Misc'] <= salary*0.25:
+        title = "Approaching the shopping limit!!"
+        message = "You have spent {} on miscellaneous purchases this month. Please stop. Your limit for the month is {}".format(round(df.at[16, 'Misc'], salary*0.25),1)
+        pync.notify(message)
+    if df.at[16, 'Misc'] > 125:
+        title = "Over budget!!"
+        message = "You have spent {} more than you were supposed to this month. STOP SPENDING!!!".format(round(df.at[16,'Misc']-(salary*0.25), 1))
+        pync.notify(message)
+
+if __name__ == "__main__":
+    Food()
+    time.sleep(7)
+    Bills()
+    time.sleep(7)
+    misc()
+
+  
